3c395cf7b8dbe2288c7d2e6c474f01becec9b803
Davit Technologies <davit@davit.ie>
2022-03-14 17:04:16 +0100

    3

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           </a>
         </h1>
         <p className={styles.description}>
-          [2] Get started by editing{" "}
+          [3] Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
